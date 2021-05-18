a5687f964c6666d98706dbda92b61ffcc14c9d66
perinze <perinzefaper@foxmail.com>
2021-05-18 14:39:23 +0800

    vertex list

---
+++ template/vlist.cpp
@@ -0,0 +1,15 @@
+int head[maxn];
+int to[maxe];
+int cost[maxe]
+int nxt[maxe];
+int cnt; // 1-ordered
+
+void add(int v, int u, int w)
+{
+    to[cnt] = u;
+    cost[cnt] = w;
+    nxt[cnt] = head[v];
+    head[v] = cnt++;
+}
+
+// for (int i = head[v]; i; i = nxt[i])
